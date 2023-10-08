b4cd351db2bec3037ac8dc5780944080d6cfaec9
Keredu <luiscortesferre@gmail.com>
2023-10-08 22:22:27 +0200

    update tkinter

--- main2.py
+++ main2.py
@@ -3,21 +3,13 @@ from diffusers import StableDiffusionPipeline
 import torch
 from PIL import Image, ImageTk
 import tkinter as tk
-from tkinter import simpledialog, messagebox
+from tkinter import simpledialog
 import tempfile
 
-# Function to create a grid of images
-def image_grid(imgs, rows, cols):
-    assert len(imgs) == rows*cols
-    w, h = imgs[0].size
-    grid = Image.new('RGB', size=(cols*w, rows*h))
-    for i, img in enumerate(imgs):
-        grid.paste(img, box=(i%cols*w, i//cols*h))
-    return grid
 
 # Tkinter GUI for displaying images and saving selected images
-class ImageWindow(tk.Toplevel):  # Changed from tk.Tk to tk.Toplevel
-    def __init__(self, master, images=None):  # Added master parameter
+class ImageWindow(tk.Toplevel):
+    def __init__(self, master, images=None):
         super().__init__(master)
         self.images = images or []
         self.canvas = tk.Canvas(self)
@@ -37,7 +29,7 @@ class ImageWindow(tk.Toplevel):  # Changed from tk.Tk to tk.Toplevel
 
     def recreate_window(self):
         self.__init__(self.master)  # Recreate the window
-    
+
     def on_key(self, event):
         if event.keysym == 'Right' and self.current_index + 1 < len(self.images):
             self.show_images(self.current_index + 1)
@@ -51,8 +43,9 @@ class ImageWindow(tk.Toplevel):  # Changed from tk.Tk to tk.Toplevel
 
 
 class ParameterForm(tk.Toplevel):
-    def __init__(self, callback):
-        super().__init__()
+    def __init__(self, master, callback):
+        super().__init__(master)
+        self.master = master
         self.callback = callback
         self.prompt_label = tk.Label(self, text="Prompt:")
         self.prompt_entry = tk.Entry(self)
@@ -76,6 +69,8 @@ class ParameterForm(tk.Toplevel):
 
         self.submit_button = tk.Button(self, text="Submit", command=self.submit)
 
+        self.status_label = tk.Label(self, text="")
+
         self.prompt_label.pack()
         self.prompt_entry.pack()
         self.height_label.pack()
@@ -87,10 +82,19 @@ class ParameterForm(tk.Toplevel):
         self.num_inference_steps_label.pack()
         self.num_inference_steps_entry.pack()
         self.submit_button.pack()
+        self.status_label.pack()
 
     def submit(self):
-        self.withdraw()  # Hide this window
-        messagebox.showinfo("Processing", "Running the pipeline...")  # Show a message
+        # Disable all input widgets and the submit button
+        for widget in (self.prompt_entry, self.height_entry, self.width_entry,
+                       self.guidance_scale_entry, self.num_inference_steps_entry,
+                       self.submit_button):
+            widget.config(state=tk.DISABLED)
+
+        # Update the status label to indicate that the pipeline is running
+        self.status_label.config(text="Running the pipeline...")
+        self.update_idletasks()  # Ensure the label text is updated immediately
+
         params = {
             "prompt": self.prompt_entry.get(),
             "height": int(self.height_entry.get()),
@@ -99,18 +103,15 @@ class ParameterForm(tk.Toplevel):
             "num_inference_steps": int(self.num_inference_steps_entry.get())
         }
         self.callback(params)
-        self.destroy()  # Close this window
 
-window = None  # Declare window as a global variable outside of the main function
 
-def main():
-    global window  # Access the global window variable inside the main function
-    root = tk.Tk()
-    root.withdraw()  # Hide the root window
-    window = ImageWindow(root)  # Create the ImageWindow instance here, outside the generate_images function
+class Application:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.withdraw()  # Hide the root window
+        self.window = None
 
-    def generate_images(params):
-        global window
+    def generate_images(self, params):
         # Image generation part
         pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
         pipe.to("cuda")
@@ -126,24 +127,24 @@ def main():
             generator=generator
         ).images
 
-        grid = image_grid(images, rows=1, cols=3)
-        grid.save(f"output_grid.png")
-
         # Convert the PIL Images to temporary files so they can be loaded by Tkinter's PhotoImage
         temp_files = [tempfile.NamedTemporaryFile(delete=False, suffix='.png') for _ in images]
         for img, temp_file in zip(images, temp_files):
             img.save(temp_file)
         temp_file_paths = [temp_file.name for temp_file in temp_files]
+
         # Check if the ImageWindow instance is still valid
-        if window is None or not window.winfo_exists():
-            window = ImageWindow(root)
-        window.images = temp_file_paths
-        window.show_images(0)
+        if self.window is None or not self.window.winfo_exists():
+            self.window = ImageWindow(self.root)
+        self.window.images = temp_file_paths
+        self.window.show_images(0)
 
+    def run(self):
+        form = ParameterForm(self.root, self.generate_images)
+        form.mainloop()
 
-    form = ParameterForm(generate_images)
-    form.mainloop()
 
 # Ensure the main function is called when the script is run directly
 if __name__ == "__main__":
-    main()
+    app = Application()
+    app.run()
